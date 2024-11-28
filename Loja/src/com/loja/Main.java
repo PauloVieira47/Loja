1f9a4b8405497428f0e5575e585245daacaa263c
pauloVieira <paulovieiranunes3@gmail.com>
2024-11-27 22:22:49 -0300

    colocando metodos e gets e sets na classe produto,testando minha main

--- Loja/src/com/loja/Main.java
+++ Loja/src/com/loja/Main.java
@@ -1,7 +1,18 @@
 package com.loja;
 
+import com.loja.modelo.Produto;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Produto produto1 = new Produto("Camiseta", 49.90, 100, "Roupas");
+        Produto produto2 = new Produto("Notebook", 2500.00, 30, "Eletrônicos");
+        Produto produto3 = new Produto("Tênis", 199.90, 50, "Roupas");
+
+        System.out.println("Produto 1: " + produto1);
+        System.out.println("Produto 2: " + produto2);
+        System.out.println("Produto 3: " + produto3);
+        System.out.println("\nProduto 1 após atualização: " + produto1);
     }
 }
--- Loja/src/com/loja/modelo/Produto.java
+++ Loja/src/com/loja/modelo/Produto.java
@@ -1,4 +1,58 @@
 package com.loja.modelo;
 
 public class Produto {
+
+    private Integer id;
+    private String nome;
+    private double preco;
+    private int quantidadeEstoque;
+    private String categoria;
+
+    // Construtores usado para criar um novo produto com os dados fornecidos.
+
+    public Produto(String nome, double preco, int quantidadeEstoque, String categoria) {
+        this.nome = nome;
+        this.preco = preco;
+        this.quantidadeEstoque = quantidadeEstoque;
+        this.categoria = categoria;
+    }
+
+    // Get e set
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public double getPreco() {
+        return preco;
+    }
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+    public int getQuantidadeEstoque() {
+        return quantidadeEstoque;
+    }
+    public void setQuantidadeEstoque(int quantidadeEstoque) {
+        this.quantidadeEstoque = quantidadeEstoque;
+    }
+    public String getCategoria() {
+        return categoria;
+    }
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+    @Override
+    public String toString() {
+        // Exibe as informações do produto
+        return "ID: " + id + " | Nome: " + nome + " | Preço: R$ " + preco + " | Estoque: " + quantidadeEstoque + " | Categoria: " + categoria;
+    }
 }
